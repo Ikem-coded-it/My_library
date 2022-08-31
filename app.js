67d97d457049c585421ac8d3b4f31318b5a7247e
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2022-08-31 16:31:11 +0100

    new author and all authors routes

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
-const expressLayouts = require("express-ejs-layouts")
-const indexRouter = require("./routes/index")
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/author");
+
  
 app.set("view engine", "ejs")
 app.set("views", __dirname + "/views")
 app.set("layout", "layouts/layout")
 app.use(expressLayouts)
 app.use(express.static("public")); 
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 module.exports = app;
